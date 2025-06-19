513ed64a4abb087bd27b70ffedfaffdf9a9e2007
Jyothsna Bhukya <jyothsnabhukya022@gmail.com>
2025-06-19 20:52:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Solution */}
     </div>
   )
 }
